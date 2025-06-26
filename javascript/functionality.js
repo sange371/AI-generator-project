d9aba4f14fe1ae66e0bddc5e30aee4a9db77b649
sange371 <zusangembaleki@icloud.com>
2025-06-26 15:49:30 +0200

    tried adding image option

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -1,9 +1,6 @@
 function aiResponse(response){
     console.log(response.data.answer);
 
-  
-    
-    
     
 
     new Typewriter("#ai-output",{
@@ -16,20 +13,20 @@ function aiResponse(response){
 function aiGenerator(event){
     event.preventDefault();
 
-    let topic=document.querySelector("#ai-input");
-    let instrunctions=document.querySelector("#ai-instructions");
-    let context="A user must be able to search anything and you must give feedback as accurate as possible , each sentence must be on a new line.";
-    let prompt=`${instrunctions.value}`;
-    let apiKey="abod483e2dfa17bb1244b3fbdft2032b";
-    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+        
+        let instrunctions=document.querySelector("#ai-instructions");
+        let prompt=`${instrunctions.value}`;
+        let sentence=document.querySelector(".ai-sentence");
+        sentence.classList.remove("ai-sentence");
+        sentence.innerHTML=`<span class="loading"> generating<span id="hourglass">  ⌛️</span></span>`;
 
-    let sentence=document.querySelector(".ai-sentence");
-    sentence.classList.remove("ai-sentence");
-    sentence.innerHTML=`<span class="loading"> generating<span id="hourglass">  ⌛️</span></span>`;
+        let context="A user must be able to search anything and you must give feedback as accurate as possible , each sentence must be on a new line.";
+        let apiKey="abod483e2dfa17bb1244b3fbdft2032b";
+        let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+      
+        axios.get(apiUrl).then(aiResponse);
+    }
 
-    axios.get(apiUrl).then(aiResponse);
-    
 
-}
 let button=document.querySelector("#ai-button");
 button.addEventListener("click",aiGenerator);
